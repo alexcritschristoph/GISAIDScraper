6623b3ee3b50eb9fd37ca8238e4880eef2c7d54f
Mohammed El-Kebir <melkebir@cs.brown.edu>
2020-03-13 12:41:50 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Requirements:
+
+* puppeteer
+* metadata.tsv file from ncov repository
+
+To run:
+
+```
+$ mkdir fasta
+$ GISAID_USER=myuser GISAID_PWD=mypassword node scrape.js
+```
+
+Sequences will be stored in `fasta/` directory.
+
+To postprocess these sequences for use with ncov, run
+
+```
+for f in fasta/*.fasta
+do
+  sed -i '' -e '$a\' $f
+done
+cat fasta/*.fasta > sequences.fasta
+sed -i -e 's/^>hCoV-19\//>/g' sequences.fasta
+sed -i -e 's/|.*$//g' sequences.fasta
+sed -i -e 's/> />/g' sequences.fasta
+sed -i -e 's/ /_/g' sequences.fasta
+```
---
+++ scrape.js
@@ -0,0 +1,57 @@
+/**
+ * @name gisaid 
+ *
+ * @desc Logs into Gisaid. Provide your username and password as environment variables when running the script, i.e:
+ * `GISAID_USER=myuser GISAID_PWD=mypassword node gisaid.js`
+ *
+ */
+
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const util = require('util');
+
+
+const file_content = fs.readFileSync("metadata.tsv").toString()
+var x = file_content.split('\n');
+
+urls = []
+for (var i=0; i<x.length; i++) {
+    y = x[i].split('\t')
+    if (i > 0 && y.length > 2) {
+      url = y[2].split("_").pop();
+      urls.push(url)
+    }
+}
+
+
+const screenshot = 'gisaid.png';
+(async () => {
+  const browser = await puppeteer.launch({headless: true})
+  const page = await browser.newPage()
+  console.log('Username: ' + process.env.GISAID_USER)
+  console.log('Password: ' + process.env.GISAID_PWD)
+  await page.goto('https://platform.gisaid.org/epi3/start/EPI_ISL/410540')
+  await page.waitForNavigation()
+  await page.type('#elogin', process.env.GISAID_USER)
+  await page.type('#epassword', process.env.GISAID_PWD)
+  await page.waitForSelector('input[value="Login"]');
+  await page.click('[value="Login"]')
+  await page.waitForNavigation()
+
+  for (url of urls) {
+    id = url.split("/").pop()
+    await page.goto("https://platform.gisaid.org/epi3/start/EPI_ISL/" + url)
+    await page.waitForNavigation({ waitUntil: 'networkidle0' })
+    //await page.waitFor(1000);
+    await page.waitForXPath("//iframe");
+    console.log('Downloading: ' + url)
+    const frame = await page.mainFrame().childFrames()[0]
+    await frame.waitForXPath("//pre");
+    const [lol] = await frame.$x("//pre")
+    const text = await frame.evaluate(element => element.textContent, lol)
+    fs.writeFileSync("fasta/" + id + ".fasta", text)
+  }
+ 
+  browser.close()
+})()
